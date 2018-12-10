ce9fb44903d2f393c75369d66b6df458eab79643
Martin Freire <martin.vallejo01@gmail.com>
2018-12-10 00:47:56 -0500

    Home class complete

--- EcoHuella/EcoHuella/Models/Home.cs
+++ EcoHuella/EcoHuella/Models/Home.cs
@@ -6,6 +6,15 @@ namespace EcoHuella.Models
 {
     public class Home
     {
+        public int Id { get; set; }
+        public Double Energy { get; set; }
+        public Double Size { get; set; }
 
+        public Double FootPrint()
+        {
+            Double r = Energy * 0.045 / 1000;
+            r += Size * 0.103716;
+            return r;
+        }
     }
 }
