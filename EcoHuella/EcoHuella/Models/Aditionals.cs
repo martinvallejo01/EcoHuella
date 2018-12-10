a45bb0299f46ecce7a68219d97ffe9d0e8ccd883
Martin Freire <martin.vallejo01@gmail.com>
2018-12-09 23:05:05 -0500

    Added food info in food class

---
+++ EcoHuella/EcoHuella/Models/Aditionals.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoHuella.Models
+{
+    public class Aditionals
+    {
+
+    }
+}
---
+++ EcoHuella/EcoHuella/Models/Food.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoHuella.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+
+        public int Fruit { get; set; }
+        public int Vegetable { get; set; }
+        public int Cow { get; set; }
+        public int Pig { get; set; }
+        public int Chicken { get; set; }
+        public int Fish { get; set; }
+        public int Dairy { get; set; }
+        public int Cigarretes { get; set; }
+        public int Beer { get; set; }
+    }
+}
