4ead540202555f1e3d3860a8de1feb4e1ca40f44
Martin Freire <martin.vallejo01@gmail.com>
2018-12-09 23:05:17 -0500

    add travel info

--- EcoHuella/EcoHuella/Models/Travel.cs
+++ EcoHuella/EcoHuella/Models/Travel.cs
@@ -6,5 +6,10 @@ namespace EcoHuella.Models
 {
     public class Travel
     {
+        public int Id;
+
+        public int Bus { get; set; }
+        public int AirPlane { get; set; }
+        public int Car { get; set; }
     }
 }
